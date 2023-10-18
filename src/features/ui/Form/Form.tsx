5137e830304b0947405f3a744892a6f60e9fa0a2
JIUPER\jiupe <jiuper52@gmail.com>
2023-10-18 20:48:39 +0300

    refactoring code

--- src/features/ui/Form/Form.tsx
+++ src/features/ui/Form/Form.tsx
@@ -1,14 +1,19 @@
 import { IonInput, IonTextarea } from "@ionic/react";
 import cnBind from "classnames/bind";
 
+import { DropDown } from "../../../shared/ui/DropDown/DropDown";
+import { DropDownDate } from "../../../shared/ui/DropDownDate/DropDownDate";
+
 import styles from "./style.module.scss";
 
 const cx = cnBind.bind(styles);
 export const Form = () => {
     return (
         <div className={cx("form")}>
+            <DropDown options={[]} />
             <IonInput value="" label="Project Name" maxlength={25} labelPlacement="floating" required />
             <IonTextarea label="Description" maxlength={150} labelPlacement="floating" />
+            <DropDownDate />
         </div>
     );
 };
