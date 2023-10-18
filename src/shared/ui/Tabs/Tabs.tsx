47d7190f136d7aaf3b1aeb272a8bf3978ca83041
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2023-10-18 22:41:07 +0300

    feat: add animation tab

---
+++ src/shared/ui/Tabs/Tabs.tsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { IonIcon } from "@ionic/react";
+import cnBind from "classnames/bind";
+
+import { useActiveClass } from "../../lib/hooks/useActiveClass";
+import { Button } from "../Button/Button";
+
+import type { TabsType } from "./tabs.type";
+
+import styles from "./style.module.scss";
+
+const cx = cnBind.bind(styles);
+export const Tabs = ({ tabsRoutes }: TabsType) => {
+    const { indexBtn, setIsIndex } = useActiveClass(0);
+
+    return (
+        <div className={cx("tabs")}>
+            {tabsRoutes.map((btn, i) => (
+                <Button className={cx("tsbs__tab")} handleClick={() => setIsIndex(i)} key={i}>
+                    {btn.href && (
+                        <Link className={cx("tabs__link", indexBtn === i && "tabs__link-isActive")} to={btn.href}>
+                            <IonIcon icon={btn.icon} size="large" />
+                        </Link>
+                    )}
+                </Button>
+            ))}
+        </div>
+    );
+};
