9365449593539764f9a1eb59fc1949d045823cc8
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2023-10-31 22:25:02 +0300

    feat: add dropcalendar component, and refactoring other form component

--- src/shared/ui/DropDownDate/DropCalendar.tsx
+++ src/shared/ui/DropDownDate/DropCalendar.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { DatetimeChangeEventDetail } from "@ionic/react";
 import { IonDatetime, IonIcon } from "@ionic/react";
 import cnBind from "classnames/bind";
 import { calendarOutline } from "ionicons/icons";
@@ -6,19 +7,43 @@ import { calendarOutline } from "ionicons/icons";
 import AD from "../../assets/icons/ArrowDown.svg";
 import { Button } from "../Button/Button";
 
+import type { IDropDownDateType } from "./DropDownDate.type";
+
 import styles from "./style.module.scss";
 
 const cx = cnBind.bind(styles);
 
-export const DropCalendar = ({ title }: { title: string }): JSX.Element => {
+export const DropCalendar = ({ title, value, handleChange, type }: IDropDownDateType): JSX.Element => {
     const [isDrop, setIsDrop] = useState<boolean>(false);
     const btnRef = useRef<HTMLButtonElement>(null);
+    const calendarRef = useRef<HTMLIonDatetimeElement>(null);
+
+    const clickOut = useCallback((e: MouseEvent) => {
+        if (
+            btnRef.current &&
+            !btnRef.current.contains(e.target as Node) &&
+            calendarRef.current &&
+            !calendarRef.current.contains(e.target as Node)
+        ) {
+            setIsDrop(false);
+        }
+    }, []);
 
     useEffect(() => {
         if (btnRef && btnRef.current) {
             btnRef.current.style.transform = isDrop ? "rotate(180deg)" : "rotate(0deg)";
         }
-    }, [isDrop]);
+
+        document.addEventListener("click", clickOut);
+
+        return () => document.removeEventListener("click", clickOut);
+    }, [clickOut, isDrop]);
+
+    const onIonChange = (e: CustomEvent<DatetimeChangeEventDetail>) => {
+        if (typeof e.detail.value === "string") {
+            handleChange(e.detail.value, type);
+        }
+    };
 
     return (
         <div className={cx("dropDate")}>
@@ -30,7 +55,7 @@ export const DropCalendar = ({ title }: { title: string }): JSX.Element => {
                 <Button forwardedRef={btnRef} className={cx("dropDate__drop")} handleClick={() => setIsDrop(!isDrop)}>
                     <IonIcon icon={AD} size="large" />
                 </Button>
-                {isDrop && <IonDatetime className={cx("dropDate__calendar")} />}
+                {isDrop && <IonDatetime ref={calendarRef} value={value} onIonChange={onIonChange} />}
             </div>
         </div>
     );
